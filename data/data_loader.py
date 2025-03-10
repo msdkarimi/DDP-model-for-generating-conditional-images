f3155a90b981a09e1845801de69d3834d92d6c74
msd <itsmasoudkarimi@gmail.com>
2025-03-11 00:04:20 +0100

    update dataloader

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,24 +1,40 @@
+import json
+import os
+
 from torch.utils.data import Dataset
 import torch
+import random
 from PIL import Image, ImageOps
+from ast import literal_eval
+
 
 class DareDataset(Dataset):
     def __init__(self, root_path, mode, transform=None):
-
         self.root_path = root_path
         self.mode = mode
         self.transform = transform
-        self.data = self.read_dataset() # TODO read dataset file via a function
+        self.dataset = self.read_dataset()  # TODO read dataset file via a function
 
     def __len__(self):
-        return len(self.data)
+        return len(self.dataset)
+
     def __getitem__(self, idx):
-        image = Image.open(self.data[idx]).convert('RGB') # TODO updated based on the data of each line
+        _raw = self.dataset[idx].split('|')
+        _file_name = str(_raw[0]).zfill(12) + '.jpg'
+        _captions = literal_eval(_raw[-1])
+        _caption_idx = random.randint(0, len(literal_eval(_captions[-1])) - 1)
+        caption = _captions[_caption_idx]
+        image = Image.open(os.path.join(self.root_path, self.mode, _file_name)).convert('RGB')
         image = ImageOps.exif_transpose(image)
-
-        caption = self.data[idx] # TODO updated based on the data of each line
-
-        return {'image':image, 'caption':caption}
+        image = self.transform(image) if self.transform is not None else image
+        return {
+            'image': image,
+            'caption': [caption]
+        }
 
     def read_dataset(self):
-        return []
+        _dataset = []
+        with open(os.path.join(self.root_path, f'{self.mode}.txt', 'r')) as f:
+            for line in f:
+                _dataset.append(line)
+        return _dataset
